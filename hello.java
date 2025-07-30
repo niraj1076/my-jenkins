5dc308986747b40f92c70abdd6f973c246944544
niraj1076 <nirajaws@gmail.com>
2025-07-30 05:43:40 +0000

    third hello

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World thsi is update file!");
+        System.out.println("Hello, World thsi is update file from jenkins!");
     }
 }
