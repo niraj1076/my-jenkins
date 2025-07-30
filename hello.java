675e19e01a609cf80887cfe599ca5cdab5ef42d3
niraj1076 <nirajaws@gmail.com>
2025-07-30 05:58:06 +0000

    7 hello

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("my name i niraj and this is a messag from jenkins");
+        System.out.println("my name i niraj ");
 	System.out.println("this is a messag from jenkins");
     }
 }
