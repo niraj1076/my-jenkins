8fb3d416264afdc1ab58a7148abe413846cae191
niraj1076 <81097516+niraj1076@users.noreply.github.com>
2025-08-10 10:43:13 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("my name i niraj ");
+        System.out.println("my name is niraj ");
 	System.out.println("this is a messag from jenkins");
     }
 }
